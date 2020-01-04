5b183ccc5a70050c1536dd014485111f85f32e56
Mindaugas <m.pazereckas@gmail.com>
2020-01-04 17:30:20 +0100

    First commit

---
+++ commentsOnlyJS/server.js
@@ -0,0 +1,31 @@
+// Setup empty JS object to act as endpoint for all routes
+// Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+/* Dependencies */
+const bodyParser = require('body-parser');
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
+// Callback to debug
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
+  
